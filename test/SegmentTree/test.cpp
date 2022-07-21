2f82d45ca55e5ca60ad126c36201a3a5c95d289c
Cristime <thecristime@outlook.com>
2022-07-21 09:24:53 +0800

    Formatted with WebKit style.

--- test/SegmentTree/test.cpp
+++ test/SegmentTree/test.cpp
@@ -12,54 +12,58 @@ int n, m;
 SegTree::T origin[SegTree::maxn];
 
 #ifdef DEBUG
-int main(int argc, char *argv[]) {
-  ios::sync_with_stdio(false);
-  // ifstream fin( argv[ 1 ] );
-  // ofstream fout( argv[ 2 ] );
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    // ifstream fin( argv[ 1 ] );
+    // ofstream fout( argv[ 2 ] );
 
-  cin >> n >> m;
-  for (int i = 1; i <= n; ++i) cin >> origin[i];
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i)
+        cin >> origin[i];
 
-  SegTree::build(origin, 1, 1, n);
+    SegTree::build(origin, 1, 1, n);
 
-  for (int i = 1; i <= m; ++i) {
-    int op, left, right, addnum;
-    cin >> op >> left >> right;
-    if (op == 1) {
-      cin >> addnum;
-      SegTree::update(left, right, addnum, 1, 1, n);
-    } else {
-      cout << SegTree::query(left, right, 1, 1, n) << endl;
+    for (int i = 1; i <= m; ++i) {
+        int op, left, right, addnum;
+        cin >> op >> left >> right;
+        if (op == 1) {
+            cin >> addnum;
+            SegTree::update(left, right, addnum, 1, 1, n);
+        } else {
+            cout << SegTree::query(left, right, 1, 1, n) << endl;
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 #endif
 
-int main(int argc, char *argv[]) {
-  ios::sync_with_stdio(false);
-  ifstream fin(argv[1]);
-  ofstream fout(argv[2]);
+int main(int argc, char* argv[])
+{
+    ios::sync_with_stdio(false);
+    ifstream fin(argv[1]);
+    ofstream fout(argv[2]);
 
-  fin >> n >> m;
-  for (int i = 1; i <= n; ++i) fin >> origin[i];
+    fin >> n >> m;
+    for (int i = 1; i <= n; ++i)
+        fin >> origin[i];
 
-  SegTree::build(origin, 1, 1, n);
+    SegTree::build(origin, 1, 1, n);
 
-  for (int i = 1; i <= m; ++i) {
-    int op, left, right;
-    SegTree::T addnum;
-    fin >> op >> left >> right;
-    if (op == 1) {
-      fin >> addnum;
-      SegTree::update(left, right, addnum, 1, 1, n);
-    } else {
-      fout << SegTree::query(left, right, 1, 1, n) << endl;
+    for (int i = 1; i <= m; ++i) {
+        int op, left, right;
+        SegTree::T addnum;
+        fin >> op >> left >> right;
+        if (op == 1) {
+            fin >> addnum;
+            SegTree::update(left, right, addnum, 1, 1, n);
+        } else {
+            fout << SegTree::query(left, right, 1, 1, n) << endl;
+        }
     }
-  }
 
-  fin.close();
-  fout.close();
+    fin.close();
+    fout.close();
 
-  return 0;
+    return 0;
 }
